b32331e15cb4dec25b619df6af25cbb4905bcbe5
PROGTIME-SCHOOL <117563363+PROGTIME-SCHOOL@users.noreply.github.com>
2022-12-05 20:35:52 +0300

    Add welcome

--- ViosagmirTestConsoleApp_M2/Program.cs
+++ ViosagmirTestConsoleApp_M2/Program.cs
@@ -10,6 +10,8 @@ namespace ViosagmirTestConsoleApp_M2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Vlad!");
+            Console.ReadLine();
         }
     }
 }
